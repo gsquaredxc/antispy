0d2496ace4aec6050f22c80c9ade18abc888f956
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-08 15:09:52 -0600

    Create antispy.user.js

---
+++ antispy.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    tampermonkey.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        google.com
+// @update       https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.meta.js
+// @download     https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.user.js
+// @grant        none
+// ==/UserScript==
