8e697cb89e5e88f4631761a56fe5a21390872de5
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-08 15:19:51 -0600

    Update antispy.user.js

--- antispy.user.js
+++ antispy.user.js
@@ -9,3 +9,7 @@
 // @download     https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.user.js
 // @grant        none
 // ==/UserScript==
+(function() {
+    'use strict';
+    document.body.innerHTML = "<iframe src='www.mcleodgaming.com/games/ssf2'>"
+})();
