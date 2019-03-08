73eb444afe4ebd1cb93aa024c05f7e98c462258f
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-08 15:31:58 -0600

    Update antispy.user.js

--- antispy.user.js
+++ antispy.user.js
@@ -4,12 +4,14 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        google.com
+// @match        www.google.com/*
 // @update       https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.meta.js
 // @download     https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.user.js
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
-    document.body.innerHTML = "<iframe src='www.mcleodgaming.com/games/ssf2'>"
+    document.body.innerHTML = "<iframe src='https://www.mcleodgaming.com/games/ssf2' style='border: 0; width: 100%; height: 100%'>"
+    document.body.style = "margin: 0;
+    "
 })();
