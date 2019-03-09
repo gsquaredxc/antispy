76663ed991ac3dc99d3ff615dfb2b346a154d899
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-08 18:51:24 -0600

    Update antispy.user.js

--- antispy.user.js
+++ antispy.user.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         Antispy
 // @namespace    antispy.tampermonkey.com
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        www.google.com/*
+// @include      https://www.google.com/?*
+// @include      https://www.google.com/
+// @include      https://www.google.com
 // @update       https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.meta.js
 // @download     https://raw.githubusercontent.com/gsquaredxc/antispy/master/antispy.user.js
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
-    document.body.innerHTML = "<iframe src='https://www.mcleodgaming.com/games/ssf2' style='border: 0; width: 100%; height: 100%'>"
-    document.body.style = "margin: 0;"
-    document.style = "height: 100%;"
+    document.head.innerHTML = ''
+    document.body.innerHTML = '<object type="application/x-shockwave-flash" data="https://www.mcleodgaming.com/flash/?f=games/SSF2Beta_v1_1_0_1b.swf" width="100%" height="100%"> <param name="movie" value="https://www.mcleodgaming.com/flash/?f=games/SSF2Beta_v1_1_0_1b.swf"> <param name="quality" value="high"> </object>'
+    document.body.style = "margin: 0; height: 100%;"
+    document.documentElement.style = 'margin: 0; height: 100%;'
 })();
