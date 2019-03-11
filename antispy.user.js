c3d220f4d9596d216fe0f4c41ba05c4e35810545
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-11 08:01:07 -0500

    Update antispy.user.js

--- antispy.user.js
+++ antispy.user.js
@@ -13,11 +13,13 @@
 // ==/UserScript==
 (function() {
     'use strict';
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = "\o/";
+        e.returnValue = confirmationMessage;
+        return confirmationMessage;
+    });
     document.head.innerHTML = ''
     document.body.innerHTML = '<object type="application/x-shockwave-flash" data="https://www.mcleodgaming.com/flash/?f=games/SSF2Beta_v1_1_0_1b.swf" width="100%" height="100%"> <param name="movie" value="https://www.mcleodgaming.com/flash/?f=games/SSF2Beta_v1_1_0_1b.swf"> <param name="quality" value="high"> </object>'
     document.body.style = "margin: 0; height: 100%;"
     document.documentElement.style = 'margin: 0; height: 100%;'
-    window.addEventListener("beforeunload", function (e) {
-        return "No";
-    });
 })();
